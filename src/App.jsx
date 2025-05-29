798873564d43a273b722647ceaedbb0ac9be3b4f
Mahmud-Dev63828 <mahmudul.hasan63828@gmail.com>
2025-05-29 23:22:44 +0600

    Day 1 , 2 problem solved

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProblemSolving from "./Components/ProblemSolving";
+
+const App = () => {
+  return (
+    <div>
+      <ProblemSolving />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/ProblemSolving.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const ProblemSolving = () => {
+  //! Problem 1: Reverse a String
+  // todo: Write a function reverseString(str) that takes a string and returns it reversed.
+  //  ? Answer
+  //   const reverseString = (str) => {
+  //     return str.split("").reverse().join("");
+  //   };
+  //   const result = reverseString("mahmud");
+  //   console.log(result);
+
+  //! Problem 2: FizzBuzz
+  /* todo: Write a function fizzBuzz(n) that prints numbers from 1 to n. 
+      For multiples of 3, print "Fizz"
+
+      For multiples of 5, print "Buzz"
+
+      For multiples of both 3 and 5, print "FizzBuzz"
+
+      Otherwise, print the number
+  */
+  //  ? Answer
+  //   const fizzBuzz = (n) => {
+  //     for (let i = 1; i <= n; i++) {
+  //       if (i % 3 == 0 && i % 5 == 0) {
+  //         console.log("FizzBuzz");
+  //       } else if (i % 3 == 0) {
+  //         console.log("Fizz");
+  //       } else if (i % 5 == 0) {
+  //         console.log("Buzz");
+  //       } else {
+  //         console.log(i);
+  //       }
+  //     }
+  //   };
+  //   fizzBuzz(30);
+  return <div>Problem solving</div>;
+};
+
+export default ProblemSolving;
